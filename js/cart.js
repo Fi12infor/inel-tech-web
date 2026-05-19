a9a23e4dd1c12b6515e03fe27a6be2393b6bf78c
Fi12 <fi12@futuroinformatico.pro>
2026-05-19 13:51:58 +0100

    funcionalidad del carrito con localstorage

--- js/cart.js
+++ js/cart.js
@@ -1,21 +1,36 @@
 const cartBtn = document.querySelector(".btn-carrito")
 const buyBtn = document.querySelectorAll(".product-buy")
 const badge = document.querySelector(".cart-badge")
-
-function getProduct() {
-    console.log(buyBtn.data.id)
-}
+let productsStorage = window.localStorage // Creamos un almacenamiento local para guardar los id de los productos y el numero de productos total que guardamos
+let numerBadgeStorage = window.localStorage
+let nBadge = 0
+let productCart = []
 
 // Redirección a la pagina del carrito
 cartBtn.addEventListener("click", () => {
     window.location.href = "./cart.html"
 })
 
-// Funcion para añadir productos al carrito
-console.log(buyBtn)
-buyBtn.forEach(boton => {
-    console.log(boton)
-    boton.addEventListener("click", () => {
-        console.log(boton.dataset.id)
-    })
+// Obtener el total de productos
+total = productsStorage.getItem("total_badge")
+
+
+console.log(typeof(total))
+
+    
+
+// Mostrar el total de productos en el carrito
+badge.textContent = total
+let cartCount = 0
+// Funcion para detectar el click en el boton de cada producto
+document.addEventListener("click", e => {
+    if (e.target.classList.contains("product-buy")) {
+        console.log(e.target.dataset.id)
+        cartCount += 1
+        total += cartCount
+        // badge.textContent = nBadge
+        numerBadgeStorage.setItem("total_badge", total)
+        productsStorage.setItem("productos", productCart)
+        badge.textContent = productsStorage.getItem("total_badge")
+    }
 })
